a956ea873c7e5fdb656cc29364750f0b55c1e2b4
Vladislav Shorodok <vladislav.shorodok@student.usv.ro>
2026-05-02 20:32:13 +0300

    Update IesireDinLabirint.java

--- src/lee/IesireDinLabirint.java
+++ src/lee/IesireDinLabirint.java
@@ -5,13 +5,14 @@ import java.io.*;
 
 public class IesireDinLabirint {
     public static class Locatie {
-        public int linie, coloana;
+        int linie, coloana;
         public Locatie(int l, int c) {
-            this.linie = l; this.coloana = c;
+            this.linie = l;
+            this.coloana = c;
         }
     }
 
-    static int[] dL = {0, 0, 1, -1};
+    static int[] dL = {0, 0, -1, 1};
     static int[] dC = {1, -1, 0, 0};
 
     public static void main(String[] args) {
@@ -24,20 +25,19 @@ public class IesireDinLabirint {
             int T = scanner.nextInt();
 
             for (int t = 0; t < T; t++) {
-                int m = scanner.nextInt();
                 int n = scanner.nextInt();
+                int m = scanner.nextInt();
                 scanner.nextLine();
 
-                int[][] labirint = new int[m][n];
+                int[][] labirint = new int[n][m];
 
-                for (int i = 0; i < m; i++) {
-                    String linieText = scanner.nextLine().trim();
-                    String procesat = linieText.replace("-", " -").replace("0", " 0 ");
+                for (int i = 0; i < n; i++) {
+                    String linie = scanner.nextLine();
+                    String procesat = linie.replace("-", " -").replace("0", " 0 ");
                     Scanner linieScanner = new Scanner(procesat);
-                    for (int j = 0; j < n; j++) {
-                        if (linieScanner.hasNextInt()) {
+                    for (int j = 0; j < m; j++) {
+                        if (linieScanner.hasNextInt())
                             labirint[i][j] = linieScanner.nextInt();
-                        }
                     }
                 }
 
@@ -45,10 +45,9 @@ public class IesireDinLabirint {
                 int startC = scanner.nextInt();
 
                 Locatie start = new Locatie(startL, startC);
+                Lee(labirint, start);
 
-                lee(labirint, start);
-
-                Locatie iesire = gasesteIesire(labirint, m, n);
+                Locatie iesire = gasesteIeasire(labirint, n, m);
 
                 if (iesire == null) {
                     writer.println("Suntem blocati in labirint.");
@@ -57,16 +56,18 @@ public class IesireDinLabirint {
                     writer.println(lungime);
                     writer.println(iesire.linie + " " + iesire.coloana);
                 }
+
             }
+
         } catch (Exception e) {
             System.out.println("Eroare: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    public static void lee(int[][] labirint, Locatie start) {
-        int m = labirint.length;
-        int n = labirint[0].length;
+    public static void Lee(int[][] labirint, Locatie start) {
+        int n = labirint.length;
+        int m = labirint[0].length;
 
         labirint[start.linie][start.coloana] = 1;
 
@@ -74,25 +75,25 @@ public class IesireDinLabirint {
         q.add(start);
 
         while (!q.isEmpty()) {
-            Locatie cur = q.poll();
+            Locatie curent = q.poll();
             for (int i = 0; i < 4; i++) {
-                int nL = cur.linie + dL[i];
-                int nC = cur.coloana + dC[i];
-                if (nL >= 0 && nL < m && nC >= 0 && nC < n && labirint[nL][nC] == 0) {
-                    labirint[nL][nC] = labirint[cur.linie][cur.coloana] + 1;
+                int nL = curent.linie + dL[i];
+                int nC = curent.coloana + dC[i];
+                if (nL >= 0 && nL < n && nC >= 0 && nC < m && labirint[nL][nC] == 0) {
+                    labirint[nL][nC] = labirint[curent.linie][curent.coloana] + 1;
                     q.add(new Locatie(nL, nC));
                 }
             }
         }
     }
 
-    public static Locatie gasesteIesire(int[][] labirint, int m, int n) {
+    public static Locatie gasesteIeasire(int[][] labirint, int n, int m) {
         Locatie cea_mai_buna = null;
         int distMin = Integer.MAX_VALUE;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean peMargine = (i == 0 || i == m - 1 || j == 0 || j == n - 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                boolean peMargine = (i == 0 || i == n - 1 || j == 0 || j == m - 1);
                 if (peMargine && labirint[i][j] > 1) {
                     if (labirint[i][j] < distMin) {
                         distMin = labirint[i][j];
